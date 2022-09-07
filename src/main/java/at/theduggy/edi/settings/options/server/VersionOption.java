644ac1fb299ba8b0343101c1689d3ce70277db19
TheDuggy <gkollegger5@outlook.com>
2022-09-07 14:57:35 +0200

    Added 3 options

---
+++ src/main/java/at/theduggy/edi/settings/options/server/VersionOption.java
@@ -0,0 +1,33 @@
+/*
+ * EDI: Extended Debug Info
+ * Copyright (C) 2022  Georg Kollegger(TheDuggy/CoderTheDuggy)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package at.theduggy.edi.settings.options.server;
+
+import at.theduggy.edi.settings.options.Option;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+
+public class VersionOption extends Option {
+    public VersionOption(String displayName, String optionInfo, String optionIdentifierName) {
+        super(displayName, optionInfo, optionIdentifierName, false);
+    }
+
+    @Override
+    public String getValue(Player player) {
+        return Bukkit.getBukkitVersion().split("-")[0] + " (" + Bukkit.getServer().getName() + ")";
+    }
+}
